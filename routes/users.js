e506badce6b650e3ae65173a0a018bf3d7bfd418
mark-marshall <mark.marshallgp@gmail.com>
2019-03-20 12:28:24 +0000

    [MVP] Added get user by ID segment

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ const routes = express.Router();
 
 const db = require('../data/helpers/userDb');
 const url = '/api/users';
+const urlByUser = '/api/users/:id';
 
 routes.use(express.json());
 
 // GET ALL USERS
-
 routes.get(url, (req, res) => {
   db.get()
     .then(users => {
@@ -18,4 +18,21 @@ routes.get(url, (req, res) => {
     });
 });
 
+// GET SPECIFIC USERS BY ID
+routes.get(urlByUser, (req, res) => {
+  const { id } = req.params;
+  db.getById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with specified ID does not exist' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'The users could not be retrieved' });
+    });
+});
 module.exports = routes;
