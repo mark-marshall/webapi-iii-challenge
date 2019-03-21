152bf0680c3a160407001675642b89e8a0b823ea
mark-marshall <mark.marshallgp@gmail.com>
2019-03-21 16:27:37 +0000

    Refactored delete all user posts and user

--- middleware.js
+++ middleware.js
@@ -1,13 +1,6 @@
 function formatName(req, res, next) {
-  // const nameSplit = req.body.name.split(' ');
-  // const capitalize = nameSplit.map(
-  //   word => word.charAt(0).toUpperCase() + word.slice(1),
-
-  // );
-  // const finalFormat = capitalize.join(' ');
   const formatting = req.body.name;
-  const finalFormatting = formatting.replace(/\b(\w)/g, char => char.toUpperCase());
-
+  const finalFormatting = formatting.replace(/\b([a-z])/gi, char => char.toUpperCase());
   req.body.name = finalFormatting;
   next();
 }
--- routes/users.js
+++ routes/users.js
@@ -79,35 +79,45 @@ routes.post(url, formatName, (req, res) => {
 // DELETE USER
 routes.delete(urlByUser, (req, res) => {
   const { id } = req.params;
-  dbUsers
-    .getUserPosts(id)
-    .then(posts => {
-      if (posts) {
-        posts.forEach(post => {
-          dbPosts
-            .remove(post.id)
-            .then()
-            .catch();
-        });
-      }
-    })
-    .then(() =>
-      dbUsers
-        .remove(id)
-        .then(count => {
-          if (count) {
-            res.status(204).json({ message: 'User successfully deleted' });
-          } else {
-            res.status(404).json({ message: 'No user exists with this id' });
-          }
-        })
-        .catch(() => {
-          res.status(500).json({ message: 'The user could not be deleted' });
-        }),
-    )
-    .catch(() => {
-      res.status(500).json({ message: 'problem deleting posts for this user' });
-    });
+  deleteAllPosts(id);
+
+  function deleteAllPosts(id) {
+    dbUsers
+      .getUserPosts(id)
+      .then(posts => {
+        if (posts.length > 0) {
+          posts.forEach(post => {
+            dbPosts
+              .remove(post.id)
+              .then(() => {})
+              .catch(() => {
+                res.status(400).json({ message: 'Error deleting user post' });
+              });
+          });
+        }
+      })
+      .catch(() => {})
+      .finally(() => removeUser(id));
+  }
+
+  function removeUser(id) {
+    dbUsers
+      .remove(id)
+      .then(count => {
+        if (count) {
+          res.status(204).send({ message: 'User successfully deleted' });
+        } else {
+          res
+            .status(400)
+            .json({ message: 'The  user you tried to delete does not exist' });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ message: 'There was a problem deleting the user' });
+      });
+  }
 });
 
 // UPDATE USER
@@ -120,7 +130,7 @@ routes.put(urlByUser, (req, res) => {
       .update(id, user)
       .then(count => {
         if (count) {
-            res.status(404).json({ message: 'User deleted successfully' });
+          res.status(404).json({ message: 'User deleted successfully' });
         } else {
           res.status(404).json({ message: 'No user exists with  this id' });
         }
