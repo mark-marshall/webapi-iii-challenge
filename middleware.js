ce9fe96a9102bdf0a7f13a1948050c14edc3ae5b
mark-marshall <mark.marshallgp@gmail.com>
2019-03-21 15:33:03 +0000

    Refactored middleware with regex

--- middleware.js
+++ middleware.js
@@ -1,10 +1,14 @@
 function formatName(req, res, next) {
-  const nameSplit = req.body.name.split(' ');
-  const capitalize = nameSplit.map(
-    word => word.charAt(0).toUpperCase() + word.slice(1),
-  );
-  const finalFormat = capitalize.join(' ');
-  req.body.name = finalFormat;
+  // const nameSplit = req.body.name.split(' ');
+  // const capitalize = nameSplit.map(
+  //   word => word.charAt(0).toUpperCase() + word.slice(1),
+
+  // );
+  // const finalFormat = capitalize.join(' ');
+  const formatting = req.body.name;
+  const finalFormatting = formatting.replace(/\b(\w)/g, char => char.toUpperCase());
+
+  req.body.name = finalFormatting;
   next();
 }
 
